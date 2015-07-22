74ce41a33bed132411fe954c8ce684e2183a1459
appdutao <827566195@qq.com>
2015-07-22 18:09:43 +0800

    显示主页面数据

---
+++ app/src/main/java/com/dutao/zhihui/pager/NewsContentPager.java
@@ -0,0 +1,34 @@
+package com.dutao.zhihui.pager;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import com.dutao.zhihui.base.BasePager;
+
+/**
+ * 每个小标题下页面Pager
+ * com.dutao.zhihui.pager
+ * Created by dutao on 2015/7/22.
+ */
+public class NewsContentPager extends BasePager {
+
+    public String url;
+    private TextView textView;
+
+    public NewsContentPager(Context context, String url) {
+        super(context);
+        this.url = url;
+    }
+
+    @Override
+    public View initView() {
+        textView = new TextView(context);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        textView.setText(url);
+    }
+}
--- app/src/main/java/com/dutao/zhihui/pager/NewsMenuPager.java
+++ app/src/main/java/com/dutao/zhihui/pager/NewsMenuPager.java
@@ -1,11 +1,21 @@
 package com.dutao.zhihui.pager;
 
 import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.TextView;
+import android.view.ViewGroup;
 
+import com.dutao.zhihui.R;
 import com.dutao.zhihui.base.BasePager;
 import com.dutao.zhihui.bean.NewsCenter;
+import com.dutao.zhihui.view.pagerindicator.TabPageIndicator;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * com.dutao.zhihui.pager
@@ -13,7 +23,16 @@ import com.dutao.zhihui.bean.NewsCenter;
  */
 public class NewsMenuPager extends BasePager {
 
-    public NewsCenter.NewsCenterItem newsCenterItem;
+    public NewsCenter.NewsCenterItem newsCenterItem;//传递过来的数据
+    public List<BasePager> newsContentPageList;//存放每个小标题下对应的BasePager
+    public List<String> newsContentTitleList;//存放滑动小标题
+
+    private MyViewPagerAdapter pagerAdapter;
+
+    @ViewInject(R.id.indicator)
+    private TabPageIndicator indicator;
+    @ViewInject(R.id.pager)
+    private ViewPager pager;
 
     public NewsMenuPager(Context context,NewsCenter.NewsCenterItem newsCenterItem) {
         super(context);
@@ -27,9 +46,11 @@ public class NewsMenuPager extends BasePager {
      */
     @Override
     public View initView() {
-        TextView textView = new TextView(context);
-        textView.setText("新闻");
-        return textView;
+        view = View.inflate(context, R.layout.frag_news,null);
+        ViewUtils.inject(this,view);
+//        TextView textView = new TextView(context);
+//        textView.setText("新闻");
+        return view;
     }
 
     /**
@@ -37,6 +58,80 @@ public class NewsMenuPager extends BasePager {
      */
     @Override
     public void initData() {
+        newsContentPageList = new ArrayList<BasePager>();
+        newsContentTitleList = new ArrayList<String>();
+        for (int i = 0; i < newsCenterItem.children.size(); i++) {
+            newsContentPageList.add(new NewsContentPager(context,newsCenterItem.children.get(i).url));
+            newsContentTitleList.add(newsCenterItem.children.get(i).title);
+        }
+        pager = (ViewPager)view.findViewById(R.id.pager);
+        if(pagerAdapter == null){
+            pagerAdapter = new MyViewPagerAdapter();
+            pager.setAdapter(pagerAdapter);
+        }else{
+            pagerAdapter.notifyDataSetChanged();
+        }
 
+        indicator.setViewPager(pager);
+
+        indicator.setCurrentItem(0);
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0) {
+                    slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+                } else {
+                    slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+                }
+                indicator.setCurrentItem(position);
+                BasePager basePager = newsContentPageList.get(position);
+                basePager.initData();
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        BasePager basePager = newsContentPageList.get(0);
+        basePager.initData();
+
+    }
+
+    private class MyViewPagerAdapter extends PagerAdapter{
+
+        //返回指针对应页面的标题头目
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return newsContentTitleList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return newsContentPageList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ((ViewPager)container).addView(newsContentPageList.get(position).getRootView());
+            return newsContentPageList.get(position).getRootView();
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager)container).removeView((View)object);
+        }
     }
 }
